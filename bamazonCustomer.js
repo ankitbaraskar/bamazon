8dab0db6bf8454454dc403a3beb81fb59c36dc4b
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-04-28 11:42:31 -0400

    added simple db chech function with console logs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,59 +8,79 @@ const connection = mysql.createConnection({
     user: "root",
     password: "root",
     database: "bamazon"
-  });
+});
 
-function displayTable(){
+function displayTable() {
 
     let tableName = 'products'
-    let columns = ['item_id','product_name','price'];
-    connection.query('select ?? from ??',[columns,tableName],(error, results)=>{
-        if(error) throw error;
+    let columns = ['item_id', 'product_name', 'price'];
+    connection.query('select ?? from ??', [columns, tableName], (error, results) => {
+        if (error) throw error;
 
         // let index = [];
         // let resObject = {};
         // resObject.Item_ID = results[0].item_id;
         console.table(results);
         // console.table(resObject);
-        connection.end();
+        // connection.end();
         askInitialQuestions();
 
     });
 }
 
-function askInitialQuestions(){
+function askInitialQuestions() {
     inquirer
-    .prompt([
-      {
-        name: "getID",
-        type: "input",
-        message: "What is the Item ID you would like to order?",
-        validate: (input) => {
-            return !isNaN(parseFloat(input)) && isFinite(input);
-        }
-      },
-      {
-        name: "getquantity",
-        type: "input",
-        message: "How many would you like?",
-        validate: (input) => {
-            return !isNaN(parseFloat(input)) && isFinite(input);
-        }
-      }
-    ]).then((answers)=>{
+        .prompt([
+            {
+                name: "getID",
+                type: "input",
+                message: "What is the Item ID you would like to order?",
+                validate: (input) => {
+                    return !isNaN(parseFloat(input)) && isFinite(input);
+                }
+            },
+            {
+                name: "getquantity",
+                type: "input",
+                message: "How many would you like?",
+                validate: (input) => {
+                    return !isNaN(parseFloat(input)) && isFinite(input);
+                }
+            }
+        ]).then((answers) => {
 
-        let ID= answers.getID;
-        let quantity = answers.getquantity;
+            let ID = answers.getID;
+            let quantity = answers.getquantity;
 
-        checkDB(ID,quantity);
+            checkDB(ID, quantity);
 
-    }).catch((error)=>{
-        console.log(error);
-    })
+        }).catch((error) => {
+            console.log(error);
+        })
 }
 
-function checkDB(ID,quantity){
-    console.log('hi');  
+function checkDB(ID, quantity) {
+
+    let searchQuantity = quantity;
+
+    let tableName = 'products';
+    let search = {item_id:ID}
+
+    connection.query('select * from ?? where ?', [tableName,search], (error, results) => {
+        if (error) throw error;
+        const stockQuantity = results[0].stock_quantity;
+
+        console.log(results);
+        
+        if (searchQuantity>stockQuantity){
+            console.log('Insufficient quantity!');
+        }
+        else if (searchQuantity <= stockQuantity){
+            console.log('you may order fosho');
+        }
+        
+        connection.end();
+    });    
 }
 
 displayTable();
